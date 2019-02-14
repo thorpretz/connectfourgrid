df786576d99c55e5db87ca618e17d0b6477f65f2
thorpretz <47151008+thorpretz@users.noreply.github.com>
2019-02-14 13:01:55 -0500

    Add files via upload

--- board.js
+++ board.js
@@ -55,32 +55,115 @@ function draw() {
 var yellowlocation = null;
 
 function yellowturn() {
-var moved = true;
+var firstmoved = true;
+var secondmove = true;
   if(yellowlocation == null){
-	while(moved) {
+	while(firstmoved) {
       var spot = randomIntFromInterval(0,6);
-      if (grid[spot][5].yellowchip == false & grid[spot][5].redchip == false){
+      if (grid[spot][5].checkhole() == false){
         grid[spot][5].yellowchip = true;
 		yellowlocation = grid[spot][5];
-		moved = false;
+		firstmoved = false;
 		}
     }
+  } else if (check3() != null){
+    check3().yellowchip = true;
   }
   else {
-	if (yellowlocation.rowL() > 0 & grid[yellowlocation.colL()][yellowlocation.rowL() - 1].checkhole() == false){
-		yellowlocation = grid[yellowlocation.colL()][yellowlocation.rowL() - 1];
-		yellowlocation.yellowchip = true;
-	}
-	else if (yellowlocation.colL() + 1 < 7 & grid[yellowlocation.colL() + 1][yellowlocation.rowL()].checkhole() == false & grid[yellowlocation.colL() + 1][yellowlocation.rowL()].Vplay()){
-		yellowlocation = grid[yellowlocation.colL() + 1][yellowlocation.rowL()];
-		yellowlocation.yellowchip = true;
-	}
-	else if (yellowlocation.colL() - 1 > 0 & grid[yellowlocation.colL() - 1][yellowlocation.rowL()].checkhole() == false & grid[yellowlocation.colL() - 1][yellowlocation.rowL()].Vplay()){
-		yellowlocation = grid[yellowlocation.colL() - 1][yellowlocation.rowL()];
-		yellowlocation.yellowchip = true;
-	}
+    for (var count = 1; count < 7; count++){
+      for (var i = -count; i <=count; i++){
+        for (var j = -count; j <= count; j++){
+          var ioff = yellowlocation.colL() + i;
+          var joff =  yellowlocation.rowL() + j;
+          if (ioff > -1 && ioff < 7 && joff > -1 && joff < 6) {
+            if(grid[ioff][joff].checkhole() == false & grid[ioff][joff].Vplay() & secondmove){
+                yellowlocation = grid[ioff][joff];
+                yellowlocation.yellowchip = true;
+                secondmove = false;
+              }
+            }
+          }
+        }
+      }
   }
- }
+}
+
+function check3() {
+//  var foundspot = false;
+  for (var x = 0; x < 7; x++){
+    for (var y = 0; y < 3; y++){
+      if (grid[x][y].checkhole() == false & grid[x][y].Vplay()){
+        if (grid[x][y + 1].yellowchip & grid[x][y + 2].yellowchip & grid[x][y + 3].yellowchip) {
+          return grid[x][y];
+        }
+      }
+    }
+  }
+  for (var y = 0; y < 6; y++){
+    for (var x = 0; x < 4; x++){
+      if (grid[x][y].checkhole() == false & grid[x][y].Vplay()){
+        if (grid[x + 1][y].yellowchip & grid[x + 2][y].yellowchip & grid[x + 3][y].yellowchip) {
+          return grid[x][y];
+        }
+      }
+    }
+  }
+  for (var x = 0; x < 3; x++){
+    for (var y = 0; y < 2; y++){
+      if (grid[x][y].checkhole() == false & grid[x][y].Vplay()){
+        if (grid[x + 1][y + 1].yellowchip & grid[x + 2][y + 2].yellowchip & grid[x + 3][y + 3].yellowchip) {
+          return grid[x][y];
+        }
+      }
+    }
+  }
+  for (var x = 4; x < 7; x++){
+    for (var y = 0; y < 3; y++){
+      if (grid[x][y].checkhole() == false & grid[x][y].Vplay()){
+        if (grid[x - 1][y + 1].yellowchip & grid[x - 2][y + 2].yellowchip & grid[x - 3][y + 3].yellowchip) {
+          return grid[x][y];
+        }
+      }
+    }
+  }
+  for (var x = 0; x < 7; x++){
+    for (var y = 0; y < 3; y++){
+      if (grid[x][y].checkhole() == false & grid[x][y].Vplay()){
+        if (grid[x][y + 1].redchip & grid[x][y + 2].redchip & grid[x][y + 3].redchip) {
+          return grid[x][y];
+        }
+      }
+    }
+  }
+  for (var y = 0; y < 6; y++){
+    for (var x = 0; x < 4; x++){
+      if (grid[x][y].checkhole() == false & grid[x][y].Vplay()){
+        if (grid[x + 1][y].redchip & grid[x + 2][y].redchip & grid[x + 3][y].redchip) {
+          return grid[x][y];
+        }
+      }
+    }
+  }
+  for (var x = 0; x < 3; x++){
+    for (var y = 0; y < 2; y++){
+      if (grid[x][y].checkhole() == false & grid[x][y].Vplay()){
+        if (grid[x + 1][y + 1].redchip & grid[x + 2][y + 2].redchip & grid[x + 3][y + 3].redchip) {
+          return grid[x][y];
+        }
+      }
+    }
+  }
+  for (var x = 4; x < 7; x++){
+    for (var y = 0; y < 3; y++){
+      if (grid[x][y].checkhole() == false & grid[x][y].Vplay()){
+        if (grid[x - 1][y + 1].redchip & grid[x - 2][y + 2].redchip & grid[x - 3][y + 3].redchip) {
+          return grid[x][y];
+        }
+      }
+    }
+  }
+  return null;
+}
 
 
 
